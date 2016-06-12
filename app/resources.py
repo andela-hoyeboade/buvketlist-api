13e03f99eaae7ba0ee0e7ed96191d43324f99091
andela-hoyeboade <hassan.oyeboade@andela.com>
2016-06-13 00:11:17 +0100

    [Chore] Removed unnecessary conditional statement

--- app/resources.py
+++ app/resources.py
@@ -146,8 +146,8 @@ class AllBucketLists(Resource):
         Retrieve all bucketlists for a particular user.
         Args:
             q: Searches bucketlists by name (optional)
-            limit: Limit number of retrieved results per page (optional)
-            page: Number of pages to contain retrieved results (optional)
+            limit: Limit number of retrieved bucketlists per page (optional)
+            page: Number of pages to contain retrieved bucketlists (optional)
         Returns:
             json: All bucketlists with their content
         '''
@@ -249,6 +249,9 @@ class AllBucketListItems(Resource):
         Get all items for a particular bucketlist.
         Args:
             id: The id of the bucketlist whose items is to be retrieved (required)
+            q: Searches bucketlist items by name (optional)
+            limit: Limit number of retrieved bucketlist items per page (optional)
+            page: Number of pages to contain retrieved bucketlist items (optional)
         Header:
             Token: Authentication Token for the User (required)
         Returns:
@@ -300,11 +303,8 @@ class SingleBucketListItem(Resource):
         '''
         bucketlist_item = BucketListItem.query.filter_by(
             id=item_id, bucketlist_id=id).first()
-        if bucketlist_item:
-            return jsonify({'items': get_single_bucketlist_item(bucketlist_item)})
-        else:
-            return messages['no_bucketlist_item'], 200
-
+        return jsonify({'items': get_single_bucketlist_item(bucketlist_item)})
+        
     @auth.user_is_login
     @auth.bucketlist_exist
     @auth.bucketlist_item_exist
